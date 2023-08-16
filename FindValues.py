fcac62c1bb196e730653b602359a1348f67098f3
Byte717 <115853289+Byte717@users.noreply.github.com>
2023-08-16 08:11:57 -0400

    Updates
    Added path control, more options.

--- FindValues.py
+++ FindValues.py
@@ -21,12 +21,13 @@ class bcolors:
 exit = False
 cam = None
 class Window(tk.Tk):
-    def __init__(self, imageDir:str = None,webcam: bool = False) -> None:
+    def __init__(self, imageDir:str = None,webcam: bool = False, showPath:bool = False) -> None:
         super().__init__()
-        self.Font_tuple = ("Comic Sans MS", 20, "bold")
-        self.Font_tuple2 = ("Comic Sans MS", 12, "bold")
+        self.Font_tuple = ("Comic Sans MS", 18, "bold")
+        self.Font_tuple2 = ("Comic Sans MS", 15, "bold")
         self.title("RGB Value Tester")
         self.config(bg="gray19")
+        self.showPath = showPath
         self.protocol("WM_DELETE_WINDOW",self.onClose)
         # self.geometry("1920x1080")
         if imageDir is None and webcam == False:
@@ -36,6 +37,7 @@ class Window(tk.Tk):
             # self.cam = cv2.VideoCapture(0)
             self.img = np.zeros((200,400,3),dtype=np.uint8)
             pass
+
         elif os.path.exists(imageDir) == False:
             print(bcolors.FAIL + bcolors.UNDERLINE + "INVALID PATH" + bcolors.ENDC)
             raise
@@ -49,9 +51,13 @@ class Window(tk.Tk):
                 self.paths = list(os.listdir(imageDir))
                 self.n = len(self.paths)
 
+                if self.showPath:
+                    text = os.path.join(self.dir,self.paths[self.imageIndex])
+                else:
+                    text = self.paths[self.imageIndex]
 
-                self.imageLabel = tk.Label(self,text=os.path.join(self.dir,self.paths[self.imageIndex]), bg="gray25",fg="White",font=self.Font_tuple)
-                self.imageLabel.pack(side=tk.TOP)
+                self.imageLabel = tk.Label(self,text=text, bg="gray25",fg="White",font=self.Font_tuple)
+                self.imageLabel.grid(row=0,column=2)
 
                 self.keyboardSetup()
                 self.MultipleInit()
@@ -61,46 +67,46 @@ class Window(tk.Tk):
         imgtk = self.parse_image(self.img)
         self.original = tk.Label(self,image=imgtk)
         self.original.image = imgtk
-        self.original.pack()
+        self.original.grid(row=1,column=1)
 
         self.display = tk.Label(self, image=imgtk)
         self.display.image = imgtk
-        self.display.pack(side=tk.TOP)
+        self.display.grid(row=1,column=3)
 
 
         self.l1 = tk.Label(self,text="Low Red Value", fg='Red',bg="gray19",font=self.Font_tuple2)
-        self.l1.pack()
+        self.l1.grid(row=2,column=1)
         self.LowRed = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
-        self.LowRed.pack()
+        self.LowRed.grid(row =3,column=1)
 
         self.l2 = tk.Label(self, text="High Red Value",fg='Red',bg="gray19",font=self.Font_tuple2)
-        self.l2.pack()
+        self.l2.grid(row=4,column=1)
         self.HighRed = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
         self.HighRed.set(255)
-        self.HighRed.pack()
+        self.HighRed.grid(row=5,column=1)
 
         self.l3 = tk.Label(self, text="Low Blue Value",fg='DodgerBlue',bg="gray19",font=self.Font_tuple2)
-        self.l3.pack()
+        self.l3.grid(row=2,column=2)
         self.LowBlue = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
-        self.LowBlue.pack()
+        self.LowBlue.grid(row=3,column=2)
 
 
         self.l4 = tk.Label(self, text="High Blue Value",fg='DodgerBlue',bg="gray19",font=self.Font_tuple2)
-        self.l4.pack()
+        self.l4.grid(row=4,column=2)
         self.HighBlue = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
         self.HighBlue.set(255)
-        self.HighBlue.pack()
+        self.HighBlue.grid(row=5,column=2)
 
         self.l5 = tk.Label(self, text="Low Green Value",fg='lawn green',bg="gray19",font=self.Font_tuple2)
-        self.l5.pack()
+        self.l5.grid(row=2,column=3)
         self.LowGreen = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
-        self.LowGreen.pack()
+        self.LowGreen.grid(row=3,column=3)
 
         self.l6 = tk.Label(self, text="High Green Value",fg='lawn Green',bg="gray19",font=self.Font_tuple2)
-        self.l6.pack()
+        self.l6.grid(row=4,column=3)
         self.HighGreen = tk.Scale(self, from_= 0, to=255, orient=tk.HORIZONTAL,command=self.callback, length=300,bg="gray35",fg="White")
         self.HighGreen.set(255)
-        self.HighGreen.pack()
+        self.HighGreen.grid(row=5,column=3)
 
 
     def keyboardSetup(self):
@@ -109,10 +115,10 @@ class Window(tk.Tk):
 
     def MultipleInit(self) -> None:
         self.leftButton = tk.Button(self, text="<-", width=10,height=5, bg="gray25",fg="White",command=self.leftPress)
-        self.leftButton.pack(side=tk.LEFT)
+        self.leftButton.grid(row= 3,column=0)
 
         self.rightButton = tk.Button(self, text="->", width=10, height=5, bg="gray25", fg="White",command=self.rightPress)
-        self.rightButton.pack(side=tk.RIGHT)
+        self.rightButton.grid(row =3, column=4)
         pass
 
 
@@ -121,13 +127,16 @@ class Window(tk.Tk):
         self.original.configure(image=parsed)
         self.original.image = parsed
         self.callback()
-    def changeImage(self, path = None):
+    def changeImage(self, path = None, image = None):
         if path is None:
             print("NO PATH PROVIDED")
             raise
         else:
-            self.imageLabel.config(text=path)
-            self.img = cv2.imread(path)
+            if self.showPath:
+                self.imageLabel.config(text=os.path.join(path,image))
+            else:
+                self.imageLabel.config(text=image)
+            self.img = cv2.imread(os.path.join(path,image))
             parsed = self.parse_image(self.img)
             self.original.configure(image=parsed)
             self.original.image = parsed
@@ -139,7 +148,7 @@ class Window(tk.Tk):
             return
         else:
             self.imageIndex -= 1
-            self.changeImage(os.path.join(self.dir, self.paths[self.imageIndex]))
+            self.changeImage(self.dir, self.paths[self.imageIndex])
 
 
     def rightPress(self, event=None):
@@ -147,7 +156,7 @@ class Window(tk.Tk):
             return
         else:
             self.imageIndex += 1
-            self.changeImage(os.path.join(self.dir, self.paths[self.imageIndex]))
+            self.changeImage(self.dir, self.paths[self.imageIndex])
 
     def parse_image(self,img) -> ImageTk.PhotoImage:
         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -185,11 +194,12 @@ def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("-dir", "--dir", help="Use images")
     parser.add_argument("-webcam","--webcam",help="Use webcam",action="store_true")
+    parser.add_argument("-absolutePath","--absolutePath",help="Show Entire paths",action="store_true")
     args = parser.parse_args()
     if args.webcam:
         cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-    window = Window(args.dir,args.webcam)
+    window = Window(args.dir,args.webcam,args.absolutePath)
 
     while not exit:
         if args.webcam:
